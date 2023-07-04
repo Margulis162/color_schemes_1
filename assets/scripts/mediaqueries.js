01e3e83ca8e3d99da47a06c0ebf29f200d4abf13
Mark <markmargulis@proton.me>
2023-07-04 11:58:28 -0400

    fixed javascript for navbar by adding reload on resize for larger screensizes

--- assets/scripts/mediaqueries.js
+++ assets/scripts/mediaqueries.js
@@ -1,16 +1,20 @@
 //this snippet suppose to change the Id on the active tub when nav bar collapse
+
 let screen_size = window.matchMedia("(max-width:768px)");
 let large_screens = window.matchMedia("(min-width:768px)");
+//function to remove styling of the active tub on collapse
 function tub_deactivator(screen_size){
     if(screen_size.matches){
         document.getElementById('menu_item_active').id ="menu_item";
         
-        console.log('works that far')
+       
     }
 }
 
+//reloads the page on screen resize when navbar extends 
 function tub_activator(large_screens){
     location.reload();
+    c
 }
 
-large_screens.addEventListener('resize',tub_activator);
+window.addEventListener('resize',tub_activator);
