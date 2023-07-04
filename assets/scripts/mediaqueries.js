a78c47c0ffa7587ca2529e9bf18c93f459d52c7c
Mark <markmargulis@proton.me>
2023-07-03 20:12:38 -0400

    I have added a js code to handle the menue css on collapse and back. In the process I have also discovered a nasty typo on page 2 and fixed it

--- assets/scripts/mediaqueries.js
+++ assets/scripts/mediaqueries.js
@@ -1,3 +1,16 @@
-function activeTubIdFixer(){
-    
+//this snippet suppose to change the Id on the active tub when nav bar collapse
+let screen_size = window.matchMedia("(max-width:768px)");
+let large_screens = window.matchMedia("(min-width:768px)");
+function tub_deactivator(screen_size){
+    if(screen_size.matches){
+        document.getElementById('menu_item_active').id ="menu_item";
+        
+        console.log('works that far')
+    }
 }
+
+function tub_activator(large_screens){
+    location.reload();
+}
+
+large_screens.addEventListener('resize',tub_activator);
